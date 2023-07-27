5652ded01c546376ced444ee189245116a5e7d0d
FabianSolheim <fabian.j.solheim@gmail.com>
2023-07-27 23:01:41 +0200

    BubbleSortint

--- module02/bubble_sort.go
+++ module02/bubble_sort.go
@@ -1,12 +1,27 @@
 package module02
 
-import "sort"
+import (
+	"sort"
+)
 
 // BubbleSortInt will sort a list of integers using the bubble sort algorithm.
 //
 // Big O: O(N^2), where N is the size of the list
-func BubbleSortInt(list []int) {
+
+
+func BubbleSortInt(list []int)  {
+	for i := 0; i < len(list); i++ {
+		
+		for j := 0; j < len(list) - i - 1; j++ {
+			if(list[j] > list[j + 1]) {
+				temp := list[j]
+				list[j] = list[j + 1]
+				list[j + 1] = temp
+			}
+		}
+	}
 }
+
 
 // BubbleSortString is a bubble sort for string slices. Try implementing it for
 // practice.
